bf2c086fc4469fe9cfea445c376ce2fa8f1092b1
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-05-07 18:18:09 +0800

    加了个宏定义用来从外部设置参数

--- inc/TSP/config.hpp
+++ inc/TSP/config.hpp
@@ -1,9 +1,19 @@
 #ifndef _tsp_config_hpp
 #define _tsp_config_hpp
+
+#ifndef TSP_MAX_POINT_NUM
+#define TSP_MAX_POINT_NUM 200
+#endif
+
+#ifndef TSP_TERM_COND
+#define TSP_TERM_COND 1e-2
+#endif
+
 namespace TSP {
 
-constexpr int MAXN = 200;
-constexpr float term_cond = 1e-2;
+constexpr int MAXN = TSP_MAX_POINT_NUM;
+constexpr float term_cond = TSP_TERM_COND;
 
 }  // namespace TSP
+
 #endif  // _tsp_config_hpp

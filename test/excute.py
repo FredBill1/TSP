27232e2099e4e86ab85698e624fc429df5345ad0
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-06-20 23:14:57 +0800

    弄了excute显示时长

--- test/excute.py
+++ test/excute.py
@@ -3,6 +3,7 @@ from os.path import join, realpath, dirname
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import numpy as np
+from time import time
 
 DIR = dirname(realpath(__file__))
 InputFile = join(DIR, "input.txt")
@@ -26,7 +27,9 @@ ax.set_ylim(0 - 0.5, LIM + 0.5)
 def run_tsp():
     verts = np.random.uniform(0, LIM, (N, 2))
     Input = f"{mode}\n{N}\n" + "\n".join(["{} {}".format(x, y) for x, y in verts])
+    start = time()
     out, err = Popen([TSP_EXE], stdin=PIPE, stdout=PIPE).communicate(Input.encode("ascii"))
+    print(f"Time: {time() - start}")
     out = out.decode("ascii").split()
     length = float(out[0])
     length_text.set_text(f"{length:.2f}")
